ab9ab25b89ee2116eb675e6b58ed0a6e2fd198c0
Arthur Fernandez <arthur@snooget.com>
2023-10-13 15:37:32 -0700

    added in log for json data, and start button event

--- popup.js
+++ popup.js
@@ -12,33 +12,27 @@ document
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const excelData = XLSX.utils.sheet_to_json(worksheet, { raw: true });
-        // console.log(excelData);
-        const filteredData = excelData.filter(
-          (row) =>
-            Object.keys(row).length > 0 && row["Individual "] !== undefined
-        );
-        console.log(filteredData);
-        // Can use or process this json data created from the function above
-        // for example:
-        if (excelData.length > 0) {
-          alert(JSON.stringify(excelData[0]));
-        }
+        console.log(excelData);
       };
 
       reader.readAsBinaryString(file);
     }
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      chrome.tabs.sendMessage(
-        activeTab.id,
-        { data: filteredData },
-        (response) => {
-          if (response && response.status === "success") {
-            console.log("Data transfered successfully.");
-          } else {
-            console.error("Data injection failed see error");
+    document.getElementById("start").addEventListener("click", () => {
+      // Below is the code that sends a message to start the injection process.
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+        // Send message to "service_worker.js" to start the injection process
+        chrome.tabs.sendMessage(
+          activeTab.id,
+          { action: "startInjection" },
+          (response) => {
+            if (response && response.status === "success") {
+              console.log("Data transfer successful");
+            } else {
+              console.error("Data injection failed. See error.");
+            }
           }
-        }
-      );
+        );
+      });
     });
   });
