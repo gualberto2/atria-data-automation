7f072228a75ee807a859c989eeb2e7bf221165d7
Arthur Fernandez <arthur@snooget.com>
2023-10-14 11:33:25 -0700

    added onupdate and new tracker for step 2 tab

--- service_worker.js
+++ service_worker.js
@@ -9,3 +9,25 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     });
   }
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && isTabOfInterest(tab)) {
+    // The tab is one we're interested in and it's fully loaded, so inject the content script into it to execute step two...
+    chrome.tabs.executeScript(
+      tabId,
+      { file: "newContentScript.js" },
+      function () {
+        // Optional callback to handle after the script is injected
+        console.log("Script injected and executed");
+      }
+    );
+  }
+});
+
+function isTabOfInterest(tab) {
+  // Check the tab's properties, like URL, to see if it's the tab you're interested in
+  // Return true if it is, false otherwise
+  return tab.url.includes(
+    "https://my.advisorlogin.com/secure/proposalapp/#/household"
+  ); // Simplified check, you might need more complex logic
+}
