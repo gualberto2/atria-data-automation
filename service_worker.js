a699573f57bc32ae56abdd8ec6dc6eeac316644b
Arthur Fernandez <arthur@snooget.com>
2023-10-28 20:15:33 -0700

    add storage retrevial for parsed data

--- service_worker.js
+++ service_worker.js
@@ -14,28 +14,34 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     ? targetUrls.some((part) => tab.url.includes(part))
     : false;
   if (changeInfo.status === "complete" && isTargetUrl) {
-    chrome.scripting.executeScript(
-      {
-        target: { tabId: tabId },
-        files: ["newTabScript.js"],
-      },
-      (injectionResults) => {
-        for (const frameResult of injectionResults) {
-          if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-            return;
-          }
-          console.log("script injected test");
-          setTimeout(() => {
-            chrome.tabs.sendMessage(tabId, {
-              action: "automateData",
-              data: {
-                //data goes here for next steps in filling out the form...
-              },
-            });
-          }, 2000); // A 2 second delay
-        }
+    // Below I will be retreiving the stored and parsed excel data...
+    chrome.storage.local.get("excelData", function (data) {
+      if (chrome.runtime.lastError) {
+        console.error("Error retreiving the data:", chrome.runtime.lastError);
+        return;
       }
-    );
+      const excelData = data.excelData; // extracted data right here...
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabId },
+          files: ["newTabScript.js"],
+        },
+        (injectionResults) => {
+          for (const frameResult of injectionResults) {
+            if (chrome.runtime.lastError) {
+              console.error(chrome.runtime.lastError.message);
+              return;
+            }
+            console.log("script injected test");
+            setTimeout(() => {
+              chrome.tabs.sendMessage(tabId, {
+                action: "automateData",
+                data: excelData, // Passing the excelData here to newTabScript.js
+              });
+            }, 2000); // A 2 second delay
+          }
+        }
+      );
+    });
   }
 });
